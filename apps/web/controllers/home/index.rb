042c802db1e4c8af4037b2068e87a484b7d0a662
Agafonov Maksim <agafonov.maksim@jetrockets.ru>
2018-11-29 18:24:20 +0300

    Add index page

--- apps/web/controllers/home/index.rb
+++ apps/web/controllers/home/index.rb
@@ -5,19 +5,20 @@ module Web::Controllers::Home
   class Index
     include Web::Action
 
+    expose :auth_uri
+
     def call(params)
-    client_secrets = Google::APIClient::ClientSecrets.load(Hanami.root.join('client.json'))
-    auth_client = client_secrets.to_authorization
-    auth_client.update!(
-      :scope => 'https://www.googleapis.com/auth/drive',
-      :redirect_uri => 'https://fathomless-taiga-36544.herokuapp.com/callback',
-      :additional_parameters => {
-        "access_type" => "offline",         # offline access
-        "include_granted_scopes" => "true"  # incremental auth
-      }
-    )
-    auth_uri = auth_client.authorization_uri.to_s
-    redirect_to auth_uri
+      client_secrets = Google::APIClient::ClientSecrets.load(Hanami.root.join('client.json'))
+      auth_client = client_secrets.to_authorization
+      auth_client.update!(
+        :scope => 'https://www.googleapis.com/auth/drive',
+        :redirect_uri => 'https://fathomless-taiga-36544.herokuapp.com/callback',
+        :additional_parameters => {
+          "access_type" => "offline",         # offline access
+          "include_granted_scopes" => "true"  # incremental auth
+        }
+      )
+      @auth_uri = auth_client.authorization_uri.to_s
     end
   end
 end
